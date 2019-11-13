a7f1ddc631b687e26daf0b9be9f650bc43d3177c
JichangJang <wlckd90@gmail.com>
2019-11-13 18:48:29 +0900

    페이징 기능 추가

--- cogether/settings/common.py
+++ cogether/settings/common.py
@@ -147,3 +147,8 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20
+}
