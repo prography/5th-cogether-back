afdbd61b5aeffd19e9190b4b3658fe87570a9a8f
JichangJang <wlckd90@gmail.com>
2019-11-16 11:48:42 +0900

    카테고리별 검색 시 페이징 기능 추가

--- event/views.py
+++ event/views.py
@@ -3,7 +3,6 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from rest_framework import viewsets
-from rest_framework import mixins
 
 from event.models import Event, Category
 from event.serializers import EventSerializer
@@ -15,21 +14,40 @@ class StandardResultsSetPagination(PageNumberPagination):
     max_page_size = 1000
 
 
-class EventViewSet(viewsets.ModelViewSet, mixins.ListModelMixin):
+class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     pagination_class = StandardResultsSetPagination
 
     def list(self, request):
         category = self.request.query_params.get('category')
+        if not category:
+            queryset = self.filter_queryset(self.get_queryset())
+
+            page = self.paginate_queryset(queryset)
+            if page is not None:
+                serializer = self.get_serializer(page, many=True)
+                return self.get_paginated_response(serializer.data)
+
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(serializer.data)
+
         if category:
             category = category_exist_or_not(category)
-            queryset = Event.objects.filter(category=category)
             if not category:
-                return Response(status=status.HTTP_404_NOT_FOUND)
-        else:
-            queryset = self.get_queryset()
-        return super(EventViewSet, self).list(request)
+                return Response(status=status.HTTP_404_NOT_FOUND)            
+            else:
+                queryset = Event.objects.filter(category=category)
+                if queryset:
+                    page = self.paginate_queryset(queryset)
+                    if page is not None:
+                        serializer = self.get_serializer(page, many=True)
+                        return self.get_paginated_response(serializer.data)
+                    serializer = self.get_serializer(queryset, many=True)
+                    return Response(serializer.data)
+                else:
+                    return Response(status=status.HTTP_404_NOT_FOUND)
+
 
 def category_exist_or_not(category):
     try:
