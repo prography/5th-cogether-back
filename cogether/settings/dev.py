e0534591cbd2ccea9d90ae87ad4acad4c702912e
JichangJang <wlckd90@gmail.com>
2019-11-16 11:43:06 +0900

    파일 서빙 url 추가

--- cogether/settings/dev.py
+++ cogether/settings/dev.py
@@ -1,1 +1,6 @@
 from .common import *
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
+}
--- cogether/urls.py
+++ cogether/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
@@ -6,3 +8,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls'), name='rest_framework'),
     path('', include('event.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
